720df1927ba43d2766d3997453cca2386f2dbb21
ElleanorLamb <elamb@cern.ch>
2022-10-06 15:50:01 +0200

    setup file change

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import ast
 def get_version_from_init():
     """Obtain library version from main init."""
     init_file = os.path.join(
-        os.path.dirname(__file__), 'bb_tools', '__init__.py'
+        os.path.dirname(__file__), 'xsuite_functions', '__init__.py'
     )
     with open(init_file) as fd:
         for line in fd:
